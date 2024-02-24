6741f82730091b75fd89ac929547d748099edd02
Mason Anderson <mta0627@utulsa.edu>
2024-02-23 22:23:27 -0600

    wrote program for problem 6

--- problem#6/fib.py
+++ problem#6/fib.py
@@ -1,6 +1,19 @@
 def fibonacci(n):
     #write your code here
-    pass
+    
+    n1 = 0
+    n2 = 1
+    for i in range(n-1):
+        n3 = n1 + n2
+        n1 = n2
+        n2 = n3
+    if n > 0:
+        return n2
+    elif n == 0:
+        return n1
+    else:
+        return -1
+    
 
 
 if __name__ == '__main__':
