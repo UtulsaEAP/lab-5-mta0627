1cfcd04c59f155ddfa57465bc35facb3e5eb72d6
Mason Anderson <mta0627@utulsa.edu>
2024-02-23 21:28:49 -0600

    wrote code for problem 3

--- Problem#3/stepCounter.py
+++ Problem#3/stepCounter.py
@@ -1,10 +1,17 @@
 def feet_to_steps(user_feet):
    #write your code here
-   pass
+   
+   return int(user_feet / 2.5)
 
 if __name__ == '__main__':
     #take input feet steps here
+   
+   user_feet = float(input())
+   
     #store it into the function
-    
+   
+   steps = feet_to_steps(user_feet)
+   
     #print your steps here
-   pass
+   
+   print (steps)
